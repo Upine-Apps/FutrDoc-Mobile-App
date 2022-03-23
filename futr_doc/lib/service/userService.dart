0583e6e2af874197aed532704038d82124357a5c
Tate Walker <tatewalker@protonmail.com>
2022-03-23 15:56:58 -0500

    conneciton service

---
+++ futr_doc/lib/service/userService.dart
@@ -0,0 +1,89 @@
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
+
+class UserService {
+  Future<Map<String, String>> getHeaders() async {
+    final prefs = await SharedPreferences.getInstance();
+    String accessToken = prefs.getString('accessToken') ?? '';
+    String idToken = prefs.getString('idToken') ?? '';
+    String refreshToken = prefs.getString('refreshToken') ?? '';
+    final _headers = {
+      "Content-Type": "application/x-www-form-urlencoded",
+      'accessToken': accessToken,
+      'idToken': idToken,
+      'refreshToken': refreshToken
+    };
+    return _headers;
+  }
+
+  static final _hostUrl = '54.91.210.147:3000/user';
+
+  UserService._privateConstructor();
+  static final UserService instance = new UserService._privateConstructor();
+
+  Future registerUser(String username, String password) async {
+    final url = '$_hostUrl';
+    var headers = await getHeaders();
+    Object body = {username: username, password: password};
+    try {
+      var data = await http.post(Uri.parse(url), headers: headers, body: body);
+      return {data};
+    } catch (err) {
+      return {'status': false};
+    }
+  }
+
+  Future authenticateUser(String username, String password) async {
+    final url = '$_hostUrl/login';
+    var headers = await getHeaders();
+    Object body = {username: username, password: password};
+    try {
+      var data = await http.post(Uri.parse(url), headers: headers, body: body);
+      return {data};
+    } catch (err) {
+      return {'status': false};
+    }
+  }
+
+  Future getEmailCode(String username, String password) async {
+    final url = '$_hostUrl/getEmailCode';
+    var headers = await getHeaders();
+    Object body = {username: username, password: password};
+    try {
+      var data = await http.post(Uri.parse(url), headers: headers, body: body);
+      return {data};
+    } catch (err) {
+      return {'status': false};
+    }
+  }
+
+  Future validateSMS(String username, String code) async {
+    final url = '$_hostUrl/validateSMS';
+    var headers = await getHeaders();
+    Object body = {username: username, code: code};
+    try {
+      var data = await http.post(Uri.parse(url), headers: headers, body: body);
+      return {data};
+    } catch (err) {
+      return {'status': false};
+    }
+  }
+
+  Future validateEmail(String username, String code) async {
+    final url = '$_hostUrl/validateEmail';
+    var headers = await getHeaders();
+    Object body = {
+      'username': username,
+      'code': code,
+      'accessToken': headers['accessToken'],
+      'idToken': headers['idToken'],
+      'refreshToken': headers['refreshToken']
+    };
+    try {
+      var data = await http.post(Uri.parse(url), headers: headers, body: body);
+      return {data};
+    } catch (err) {
+      return {'status': false};
+    }
+  }
+}
